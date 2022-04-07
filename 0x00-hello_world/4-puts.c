887905c5c9a84f39658ad8b93f24b6f9a327a9aa
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-07 07:13:33 -0700

    hello puts fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	/*TODO:A program that uses puts to display on the standard output*/
 	puts("\"Programming is like building a multilingual puzzle"); 
-	return 0;
+	return (0);
 }
