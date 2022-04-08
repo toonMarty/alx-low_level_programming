e32b2a8899d7ece108074f7ccdf0c37dbe55a129
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-08 11:40:01 -0700

    bases coded

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+	int b = '0';
+
+	while (a <= 'f')
+	{
+		if (b <= 9)
+		{
+			putchar(b + '0');
+			b++;
+		}
+		else
+		{
+			putchar(a);
+			a++;
+		}
+		putchar('\n');
+
+		return (0);
+	}
+}
