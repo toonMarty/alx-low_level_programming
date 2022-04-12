61a3d1ef92aedd0e7f3c4f375d3f095b8ad664cf
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-12 06:48:06 -0700

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+
+	while (b <= 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
