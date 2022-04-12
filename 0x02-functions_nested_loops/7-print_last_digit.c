1f07b4c0010a8c67a3f99d615245a222e4d34da0
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-12 10:26:49 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	if (n < 0)
+	{
+		return (-n % 10);
+	}
+	else
+	{
+		return (n % 10);
+	}
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-putchar. A c program that prints _putchar followed by a new line
 1-alphabet.c A c program that prints the alphabet in lower case
+2
