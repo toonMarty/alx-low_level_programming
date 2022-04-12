ab2cdad4f428f3573f83f26372ade6e7932ee5c4
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-12 08:34:01 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: a number for which we find the last digit
+ *
+ * Return: n % 10 (Success)
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
