fb25bb555443c0f52fcdaf627bd7241c4ea63450
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-13 10:34:37 -0700

    task 02

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,6 +14,8 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
+if ((a != b && a!=c) || (b != c && b != a) || (c != a && c != b))
+{
 if ((a > b) && (a > c))
 {
 largest = a;
@@ -26,6 +28,10 @@ else
 {
 largest = c;
 }
-
+}
+else
+{
+return -1;
+}
 return (largest);
 }
