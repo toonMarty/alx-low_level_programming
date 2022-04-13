ceadd39389a70e7d5ea37b97cbf424f6719f4979
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-13 01:54:24 -0700

    last digit fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10 + (n % 10) * 10);
+	int last_digit = n % 10;
+
+	if(last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
