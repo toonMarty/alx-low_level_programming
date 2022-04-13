1cd228736e83a1f8c0ffa4db94e25f677f98ec83
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-13 03:26:39 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int j, sum = 0;
+for (j = 0; j < 1024; j++)
+{
+if ((j % 3) == 0 || (j % 5) == 0);
+sum += j;
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
+
