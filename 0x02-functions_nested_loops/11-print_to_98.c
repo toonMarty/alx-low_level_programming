757d594dfebcc86ca1923efdfaeaecf2ef3612e8
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-13 02:47:56 -0700

    n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_to_98- Entry point
+ *
+ * @n: a number used to count
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+			printf("%d\n", n);
+		}
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+			printf("%d\n", n);
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1-alphabet.c A c program that prints the alphabet in lower case
 8-24_hours. A c program that counts hours and minutes for the entire day
 9-times_table. A c program that prints a multiplication table upto 9
+11-print_to_98. A c program that prints all matural numbers from n to 98
