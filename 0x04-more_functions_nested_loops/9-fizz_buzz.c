e6ee0229aa18313687ca0fac27cd7921c335fcb1
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 08:48:19 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+int a = 1;
+
+while (a <= 100)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", a);
+}
+a++;
+}
+
+return (0);
+}
