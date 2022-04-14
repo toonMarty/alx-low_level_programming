e4a79111b6d92fbd2ff819ba04b266f5d8c30cd7
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 09:29:12 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
  *
  * @c: ASCII code for character
  *
- * Return - 1 if uppercase and 0 otherwise
+ * Return: 1 if uppercase and 0 otherwise
  */
 int _isupper(int c)
 {
@@ -12,6 +12,7 @@ if (c >= 65 && c <= 90)
 {
 return (1);
 }
+
 else
 {
 return (0);
