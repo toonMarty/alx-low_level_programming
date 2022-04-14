555806f7905e93ce66b4bcac1614410b96ebd670
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 03:09:54 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Function that multiplies two numbers and returns their result
+ *
+ * @a: First integer
+ *
+ * @b: Second integer
+ *
+ * Return - The product of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 task -0. _isupper. A c program that returns 1 if uppercase and 0 otherwise
 task-1. A c program that checks whether a character is a digit
+task-2. A c program that multiplies two numbers and returns the result
