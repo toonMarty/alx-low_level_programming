788eb1ac1f3094dc96b3d2293ab1b5cece2fb695
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 05:48:54 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Function that prints n number of lines
+ * @n: number of lines to be printed
+ * Return - nothing
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+int i = 0;
+while (i <= n)
+{
+_putchar('_');
+i++;
+}
+_putchar('\n');
+}
+}
