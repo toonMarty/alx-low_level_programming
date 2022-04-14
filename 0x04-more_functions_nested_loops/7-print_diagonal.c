e9ed756489d16609c6d2c9a6a456510d6dfdbc47
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 08:12:18 -0700

    digonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function that prints diagonals
+ * @n: number of diagonals to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
