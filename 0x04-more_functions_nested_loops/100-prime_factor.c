4fc31c3bd2bd1704485409470aed8085c99565e9
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 09:40:00 -0700

    prime time

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+unsigned long n, i, max;
+
+n = 612852475143;
+max = 1;
+
+for (i = 3; i <= n; i+=2)
+{
+while (n %1 == 0)
+{
+max = n;
+n = n / i;
+}
+}
+printf("%lu\n", max);
+return (0);
+}
