fdd6666bd70bc0dbd0c1e10cf7560b4791052627
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-14 09:30:32 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
  * mul - Function that returns the product of two numbers
  * @a: First integer
  * @b: Second integer
- * Return - The product of a and b i.e a * b
+ * Return: The product of a and b i.e a * b
  */
 int mul(int a, int b)
 {
