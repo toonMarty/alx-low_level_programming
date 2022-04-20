e1154982166fa56cca794a9a0f012d3eaec84cfc
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-20 06:40:34 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Entry point
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+char *cp;
+cp = s;
+
+while(*cp++)
+{
+continue;
+}
+return (cp - s - 1);
+}
+/**
+ * rev_string - Entry point
+ * @s: string
+ * Return: nothing
+*/
+void rev_string(char *s)
+{
+int len = _strlen(s);
+
+for (int i = len; i >= 0;i--)
+{
+printf("%c", *(s + i));
+}
+putchar('\n');
+}
