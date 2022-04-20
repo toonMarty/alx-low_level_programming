fc5c37d9e105be9bb9793d70b421e6eface7a9af
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-20 07:31:55 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -23,12 +23,25 @@ return (cp - s - 1);
 */
 void rev_string(char *s)
 {
-int len = _strlen(s);
-int i;
+int l, i;
+char *begin_ptr, *end_ptr, ch;
 
-for (i = len; i >= 0; i--)
-{
-printf("%c", *(s + i));
-}
-putchar('\n');
+    l = _strlen(s);
+
+    begin_ptr = s;
+    end_ptr = s;
+
+    for (i = 0; i < l - 1; i++){
+        end_ptr++;
+    }
+
+    for (i = 0; i < l / 2; i++) {
+
+        ch = *end_ptr;
+        *end_ptr = *begin_ptr;
+        *begin_ptr = ch;
+
+        begin_ptr++;
+        end_ptr--;
+    }
 }
