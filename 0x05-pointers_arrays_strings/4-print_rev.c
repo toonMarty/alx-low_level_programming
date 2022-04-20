c1e04dbfa58be8637d2061055b7858175272e22c
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-20 07:52:24 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -26,26 +26,11 @@ return (cp - s - 1);
  */
 void print_rev(char *s)
 {
-int l, i;
-    char *begin_ptr, *end_ptr, ch;
-  
-    l = _strlen(s);
+int len = _strlen(s);
 
-    begin_ptr = s;
-    end_ptr = s;
-  
-    for (i = 0; i < l - 1; i++){
-        end_ptr++;
-    }
-
-    for (i = 0; i < l / 2; i++) {
-  
-        ch = *end_ptr;
-        *end_ptr = *begin_ptr;
-        *begin_ptr = ch;
-        
-        begin_ptr++;
-        end_ptr--;
-    }
-    putchar('\n');
+for (int i = len - 1; i >= 0; --i)
+{
+printf("%c", *(s+i));
+}
+putchar('\n');
 }
