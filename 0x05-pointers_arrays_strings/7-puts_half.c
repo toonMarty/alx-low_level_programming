733e5b6a2dcddfd68381613619c45e9dd0118ef3
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-20 10:28:14 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strlen - Entry point
+ * @s: string to find length
+ * Return: length
+ */
 int _strlen(char *s)
 {
 char *cp;
@@ -11,12 +16,16 @@ continue;
 }
 return (cp - s - 1);
 }
-
+/**
+ * puts_half - Entry point
+ * @str: string
+ * Return: nothing
+ */
 void puts_half(char *str)
 {
 int len = _strlen(str);
-
-for (int i = len / 2; i < len; i++)
+int i;
+for (i = len / 2; i < len; i++)
 {
 printf("%c", *(str + i));
 }
