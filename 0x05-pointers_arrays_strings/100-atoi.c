cc04724660d5615fabce71fa259e4b2d8071fd48
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-21 06:46:51 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,42 @@
 #include "main.h"
 #include <ctype.h>
 #include <stdlib.h>
+/**
+ * _atoi - Function that converts string to integer
+ * @s: the string 
+ * Return: an integer
+ */
 int _atoi(char *s)
 {
-	char c;
-	int	a;
-	unsigned char sign;
+char c;
+int a;
+unsigned char sign;
 
 skipws:
-	c = *s;
-	if(c == ' ' || c == '\t') {
-		s++;
-		goto skipws;
-	}
-	a = 0;
-	sign = 0;
-	if(c == '-') {
-		sign++;
-		s++;
-	} else if(c == '+')
-		s++;
+c = *s;
+if (c == ' ' || c == '\t')
+{
+s++;
+goto skipws;
+}
+a = 0;
+sign = 0;
+if (c == '-')
+{
+sign++;
+s++;
+}
+else if (c == '+')
+s++;
 conv:
-	c = *s;
-	if(isdigit(c)) {
-		a = a*10 + (c - '0');
-		s++;
-		goto conv;
-	}
-	if(sign)
-		return -a;
-	return a;
+c = *s;
+if (isdigit(c))
+{
+a = a * 10 + (c - '0');
+s++;
+goto conv;
+}
+if (sign)
+return (-a);
+return (a);
 }
