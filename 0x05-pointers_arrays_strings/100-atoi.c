654c6c0518a4c6825f03d86f607e93db3c747c49
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-21 06:07:04 -0700

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+int _atoi(char *s)
+{
+	char c;
+	int	a;
+	unsigned char sign;
+
+skipws:
+	c = *s;
+	if(c == ' ' || c == '\t') {
+		s++;
+		goto skipws;
+	}
+	a = 0;
+	sign = 0;
+	if(c == '-') {
+		sign++;
+		s++;
+	} else if(c == '+')
+		s++;
+conv:
+	c = *s;
+	if(isdigit(c)) {
+		a = a*10 + (c - '0');
+		s++;
+		goto conv;
+	}
+	if(sign)
+		return -a;
+	return a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 void puts_half(char *str);
+int _atoi(char *s);
