82bfeb42130f9b4edb907f2dabbad6bf297e4923
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-21 23:20:43 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,35 +8,11 @@
  */
 int _atoi(char *s)
 {
-char c;
-int a;
-unsigned char sign;
-
-skipws:
-c = *s;
-if (c == ' ' || c == '\t')
+int x = 0;
+int i;
+for (i = 0; str[i] != '\0'; ++i)
 {
-s++;
-goto skipws;
+x = x * 10 + str[i] - '0';
 }
-a = 0;
-sign = 0;
-if (c == '-')
-{
-sign++;
-s++;
-}
-else if (c == '+')
-s++;
-conv:
-c = *s;
-if (isdigit(c))
-{
-a = a * 10 + (c - '0');
-s++;
-goto conv;
-}
-if (sign)
-return (-a);
-return (a);
+return (x);
 }
