202e39fbe7115b74e2ec180fad1c4c667940dc76
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-24 10:37:07 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-
-for (i = 0; s1[i] && s2[i]; ++i)
+int i = 0;
+while ((*s1 != '\0') || (*s2 != '\0'))
 {
-if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
-continue;
-else
-break;
-}
-if (s1[i] == s2[i])
-return (0);
-
-if ((s1[i] | 32) < (s2[i] | 32))
-return (-1);
+if (*s1 > *s2)
 return (1);
+if (*s1 < *s2)
+return (-1);
+s1++;
+s2++;
+}
+return (0);
 }
