f359bb257202a4fbbe729d033ff2224862d4a717
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-26 10:44:53 -0700

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets value of pointer to char
+ * @s: value to update
+ * @to: value to assign
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
