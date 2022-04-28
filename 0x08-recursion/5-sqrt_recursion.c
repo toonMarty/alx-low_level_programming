309adefe8f4cf07b10018c75dcf7089db3a08532
Martin Nyaga <martinnyaga7373@gmail.com>
2022-04-28 09:02:07 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: if no natural square root, return -1 else square root
+ */
+int _sqrt_recursion(int n)
+{
+return (findsqrt(n, 1));
+}
