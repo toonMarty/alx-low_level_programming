9b019b1a1196eb93b5dc6bb54403ac4f18c9e194
Martin Nyaga <martinnyaga7373@gmail.com>
2022-05-04 04:30:43 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+printf("%s\n", argv[0]);
+return (0);
+}
