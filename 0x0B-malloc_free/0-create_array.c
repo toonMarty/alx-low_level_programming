3a2377763d38e24a8cb63dead0c94b9c3744d2b7
Martin Nyaga <martinnyaga7373@gmail.com>
2022-05-06 04:11:09 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ * @size: size of array
+ * @c: char to initialize
+ * Return: pointer to char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *cPtr;
+
+cPtr = malloc(size * sizeof(char));
+if (size == 0)
+{
+return (NULL);
+}
+
+if (cPtr == NULL)
+{
+printf("Failed to allocate memory\n");
+}
+return (cPtr);
+
+free(cPtr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+malloc tasks
+task 0. Creates an array of size n initialized with a specific character
