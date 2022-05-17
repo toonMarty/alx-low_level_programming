8f0a57bc248f5949a505ceff061bb62f4bb7eda2
Martin Nyaga <martinnyaga7373@gmail.com>
2022-05-17 10:06:51 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: format of data type to be printed
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int counter = 0;
+	char *s, *separator = "";
+	va_list va;
+
+	va_start(va, format);
+	if (format)
+	{
+		while (format[counter])
+		{
+			switch (format[counter])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(va, int));
+					break;
+				case 'i':
+					printf("%s%i", separator, va_arg(va, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(va, double));
+					break;
+				case 's':
+					s = va_arg(va, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", separator, s);
+					break;
+				default:
+					counter++;
+					continue;
+			}
+			separator = ", ";
+			counter++;
+		}
+	}
+	printf("\n");
+	va_end(va);
+}
+
