48da45f54df01b3d4fff4671390adee82220d982
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-08 13:00:30 -0700

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - adds elements to a list
+ * @head: head of the list
+ * @n: element to add to list
+ * Return: address of element
+ * else return NULL
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *current;
+
+if (!head || !current)
+{
+return (NULL);
+}
+
+current = malloc(sizeof(*current));
+current->next = NULL;
+current->prev = NULL;
+current->n = n;
+
+if (*head == NULL)
+{
+*head = current;
+return (NULL);
+}
+
+current->next = *head;
+(*head)->prev = current;
+*head = current;
+
+return (*head);
+}
