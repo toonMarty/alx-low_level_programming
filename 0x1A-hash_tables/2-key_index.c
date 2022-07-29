323859f468bd907388779e31711c3276054174e3
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-29 10:24:36 -0700

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * h(key) = key mod size
+ * @key: the key
+ * @size: size of the hash table
+ * Return: index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int index = hash_djb2(key) % size;
+
+	return (index);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,3 +1,4 @@
 This directory contains tasks on hash tables
 Task 0: creates a hash table
 Task 1: algorithm that implements the djb2 hash function algorithm
+Task 2: function that returns the index of a key in a hash table
