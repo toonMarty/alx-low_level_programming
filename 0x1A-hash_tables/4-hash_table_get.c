2da8a6fbbf54c3225f07e29151aa15a536134749
toonMarty <martinnyaga7373@gmail.com>
2022-12-22 19:16:22 +0000

    Betty compliant

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * @hash_table_get - function that returns the value associated with a key
+ * hash_table_get - function that returns the value associated with a key
  * @ht: pointer to the hash table
  * @key: key to be hashed
  * Return: pointer to the value associated with the key or NULL if not found
@@ -11,6 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
 	unsigned long int index;
+
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
