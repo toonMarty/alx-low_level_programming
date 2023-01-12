d212c2fe3743bb62bdcee393a83e76f33900e856
toonMarty <martinnyaga7373@gmail.com>
2023-01-12 09:40:47 +0000

    added data structure definitions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,35 @@
 #define SEARCH_ALGOS_H
 
 #include <stdlib.h>
+#include <math.h>
+/**
+ * struct listint_s - singly linked list
+ * 
+ * @n: Integer 
+ * @index: Index of the node in the list
+ * @next: pointer to the next node 
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an
+ * express lane
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @express: Pointer to the next node in the express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
